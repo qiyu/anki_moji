f4dab6252267784be4abe92ff6b3f2dde7bda9c2
qiyu <qiyu@souche.com>
2021-06-24 18:30:14 +0800

    init

--- mojidicttool/mojidict_server.py
+++ mojidicttool/mojidict_server.py
@@ -3,17 +3,20 @@
 # Created by yu.qi on 2021/03/16.
 # Mail:yu.qi@qunar.com
 
-from pydash import get
-from requests import post
+import requests
 
+import utils
 from mojidicttool.model import Word
 
 URL_COLLECTION = 'https://api.mojidict.com/parse/functions/folder-fetchContentWithRelatives'
 URL_TTS = 'https://api.mojidict.com/parse/functions/fetchTts_v2'
+URL_LOGIN = 'https://api.mojidict.com/parse/login'
+
 CLIENT_VERSION = 'js2.12.0'
-SESSION_TOKEN = 'r:b413ba29930103a860f1b6aef7bec60e'
 APPLICATION_ID = 'E62VyFVLMiW7kvbtVq3p'
-INSTALLATION_ID = '7d959a18-48c4-243c-7486-632147466544'
+INSTALLATION_ID = ''
+
+SESSION_TOKEN = None
 
 
 def fetch_all_from_server():
@@ -29,27 +32,48 @@ def fetch_all_from_server():
 
 
 def fetch_from_server(page_index):
-    r = post(URL_COLLECTION,
-             json={"fid": "", "pageIndex": page_index, "count": 30, "sortType": 0, "_SessionToken": SESSION_TOKEN,
-                   "_ApplicationId": APPLICATION_ID, "_InstallationId": INSTALLATION_ID,
-                   "_ClientVersion": CLIENT_VERSION})
+    r = requests.post(URL_COLLECTION,
+                      json={"fid": "", "pageIndex": page_index, "count": 30, "sortType": 0,
+                            "_SessionToken": SESSION_TOKEN,
+                            "_ApplicationId": APPLICATION_ID, "_InstallationId": INSTALLATION_ID,
+                            "_ClientVersion": CLIENT_VERSION})
     data = (r.json())
-    rows = get(data, 'result.result')
+    rows = utils.get(data, 'result.result')
     mojiwords = []
     for row in rows:
-        target = (get(row, 'target'))
+        target = (utils.get(row, 'target'))
         if row['targetType'] != 102:
             continue
-        word = Word(row['title'], row['targetId'], row['targetType'], get(target, 'excerpt'), get(target, 'spell'),
-                    get(target, 'accent'),
-                    get(target, 'pron'), 0, None, None, None)
+        word = Word(row['title'], row['targetId'], row['targetType'], utils.get(target, 'excerpt'),
+                    utils.get(target, 'spell'),
+                    utils.get(target, 'accent'),
+                    utils.get(target, 'pron'), 0, None, None, None)
         mojiwords.append(word)
     return mojiwords
 
 
 def get_tts(tar_id, tar_type):
-    r = post(URL_TTS,
-             json={'tarId': tar_id, 'tarType': tar_type, "_SessionToken": SESSION_TOKEN,
-                   "_ApplicationId": APPLICATION_ID, "_InstallationId": INSTALLATION_ID,
-                   "_ClientVersion": CLIENT_VERSION})
-    return get(r.json(), 'result.result.url')
+    r = requests.post(URL_TTS,
+                      json={'tarId': tar_id, 'tarType': tar_type, "_SessionToken": SESSION_TOKEN,
+                            "_ApplicationId": APPLICATION_ID, "_InstallationId": INSTALLATION_ID,
+                            "_ClientVersion": CLIENT_VERSION})
+    return utils.get(r.json(), 'result.result.url')
+
+
+def ensure_login():
+    if not SESSION_TOKEN:
+        raise Exception('未登录')
+
+
+def login(username, password):
+    r = requests.post(URL_LOGIN, json={
+        'username': username,
+        'password': password,
+        "_ApplicationId": APPLICATION_ID,
+        "_InstallationId": INSTALLATION_ID,
+        "_ClientVersion": CLIENT_VERSION
+    })
+    return utils.get(r.json(), 'sessionToken')
+
+
+print(login('qiyu.one@gmail.com', '11301127'))
---
+++ utils.py
@@ -0,0 +1,8 @@
+def get(obj: dict, path: str):
+    field_names = path.split('.')
+    current = obj
+    for field_name in field_names:
+        current = current.get(field_name)
+        if current is None:
+            return None
+    return current

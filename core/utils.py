72755d354d3e265b8d4e010fecc45dab1d9548c5
qiyu <qiyu@souche.com>
2021-06-26 19:08:13 +0800

    init

--- core/utils.py
+++ core/utils.py
@@ -5,18 +5,18 @@ from aqt import mw
 
 def get_config():
     # Load config from config.json file
-    if not mw:
-        return {}
-    config = mw.addonManager.getConfig(get_module_name())
-    if config is None:
-        return {}
-    return config
-
+    # if not mw:
+    #     return {}
+    # config = mw.addonManager.getConfig(get_module_name())
+    # if config is None:
+    #     return {}
+    # return config
+    return {}
 
 def update_config(config: dict):
     if not mw:
         return
-    mw.addonManager.writeConfig(get_module_name(), config)
+    # mw.addonManager.writeConfig(get_module_name(), config)
 
 
 def get(obj: dict, path: str):
@@ -27,13 +27,3 @@ def get(obj: dict, path: str):
         if current is None:
             return None
     return current
-
-
-def get_module_name():
-    return __name__.split(".")[0]
-
-
-def get_addon_dir():
-    root = mw.pm.addonFolder()
-    addon_dir = os.path.join(root, get_module_name())
-    return addon_dir

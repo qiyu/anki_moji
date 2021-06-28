3e1a2b761ad7bea2ee404aecc755c138380be890
qiyu <qiyu@souche.com>
2021-06-28 09:49:27 +0800

    init

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ def download_all():
     server = MojiServer()
     server.login('qiyu.one@gmail.com', '11301127')
 
-    new_mojiwords = server.fetch_all_from_server()
-    for r in new_mojiwords:
+    words = []
+    for r in server.fetch_all_from_server():
         file_path = os.path.join(TTS_DIR_PATH, 'moji_' + r.target_id + '.mp3')
         if not os.path.lexists(file_path):
             tts_url = server.get_tts_url(r)
             storage.save_tts_file(file_path, tts_url)
         if r.accent is None:
             r.accent = ''
+        words.append(r)
     with open('/Users/qiyu/Downloads/moji.txt', 'w') as f:
         f.write('\n'.join(
             [format_row(r)
              for r in
-             new_mojiwords]))
+             words]))
 
 
 def format_row(r):

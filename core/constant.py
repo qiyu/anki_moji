0335002527717af62656ffcae26a15acdcb04595
qiyu <qiyu.one@gmail.com>
2021-10-20 11:18:42 +0800

    删除多余的代码

--- core/constant.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-# Created by yu.qi on 2021/03/19.
-# Mail:yu.qi@qunar.com
---
+++ readme.md
@@ -0,0 +1,4 @@
+发布
+```bash
+zip -r MojiToAnki.ankiaddon core __init__.py main.py manifest.json
+```

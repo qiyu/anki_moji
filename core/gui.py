d3ac68430d08ff86ef42a764d5a4bb2ec8a7425f
qiyu <qiyu@souche.com>
2022-12-29 21:57:14 +0800

    修复弹窗异常

--- core/gui.py
+++ core/gui.py
@@ -133,8 +133,24 @@ class ImportWindow(QDialog):
 
         utils.update_config(
             {'model_name': model_name, 'deck_name': deck_name, 'dir_id': dir_id, 'recursion': recursion})
+
         if model_name and deck_name:
-            self.word_loader = WordLoader(self.moji_server, self.busy_signal, self.log_signal,
+            if common.no_anki_mode:
+                model = None
+            else:
+                from aqt import mw
+                model = anki.prepare_model(model_name, deck_name, mw.col)
+                # 处理历史版本的noteType字段数据
+                anki.update_model_fields(model, mw.col)
+                # 处理历史版本的模板数据
+                if anki.update_template(model, mw.col):
+                    reply = QMessageBox.question(self, '', '插件将会自动更新对应的卡片模板，是否继续？')
+                    if reply == QMessageBox.Yes:
+                        anki.update_template(model, mw.col, force=True)
+                    else:
+                        return
+
+            self.word_loader = WordLoader(self.moji_server, self.busy_signal, self.log_signal, model,
                                           model_name, deck_name, dir_id, recursion)
             self.thread_pool.start(self.word_loader)
         else:
@@ -148,11 +164,13 @@ class ImportWindow(QDialog):
 
 
 class WordLoader(QRunnable):
-    def __init__(self, moji_server: MojiServer, busy_signal, log_signal, model_name, deck_name, dir_id, recursion):
+    def __init__(self, moji_server: MojiServer, busy_signal, log_signal, model, model_name, deck_name, dir_id,
+                 recursion):
         super().__init__()
         self.moji_server = moji_server
         self.busy_signal = busy_signal
         self.log_signal = log_signal
+        self.model = model
         self.model_name = model_name
         self.deck_name = deck_name
         self.dir_id = dir_id
@@ -170,20 +188,7 @@ class WordLoader(QRunnable):
         self.interrupted = True
 
     def save_words(self):
-        if common.no_anki_mode:
-            model = None
-        else:
-            from aqt import mw
-            model = anki.prepare_model(self.model_name, self.deck_name, mw.col)
-            # 处理历史版本的noteType字段数据
-            anki.update_model_fields(model, mw.col)
-            # 处理历史版本的模板数据
-            if anki.update_template(model, mw.col):
-                reply = QMessageBox.question(mw, '', '插件将会自动更新对应的卡片模板，是否继续？')
-                if reply == QMessageBox.Yes:
-                    anki.update_template(model, mw.col, force=True)
-                else:
-                    return
+        model = self.model
 
         self.log_signal.emit('')
 
